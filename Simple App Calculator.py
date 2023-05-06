57ebc086aaf3a73dfe396ce0220cd1ab9902f183
Ella <ellacalugay@gmail.com>
2023-05-06 16:21:41 +0800

    Pseudocode

--- Simple App Calculator.py
+++ Simple App Calculator.py
@@ -1,1 +1,11 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #5 | Simple App Calculator
+
+#Pseuodocode
+# Ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication and Division).
+# Ask the user for two numbers
+# Perform the calculation based on the operation that the user wants
+# Display the result
+# Ask if the user wants to try again or not.
+# If yes, repeat the process.
+# If no, Display “Thank you!” and exit the program.
+# Use Python Function and appropriate Exceptions to capture errors during runtime.
