483cb4215f5d539c3d7d080c9bf89b117be33cde
Ella <ellacalugay@gmail.com>
2023-05-06 17:01:15 +0800

    If the user chose addition, add the two numbers.

--- Simple App Calculator.py
+++ Simple App Calculator.py
@@ -8,6 +8,9 @@ print("Kindly input two numbers.")
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 # Perform the calculation based on the operation that the user wants
+if operation == "Addition":
+    # If the operation is Addition, add the two numbers.
+    result = num1 + num2
 # Display the result
 # Ask if the user wants to try again or not.
 # If yes, repeat the process.
