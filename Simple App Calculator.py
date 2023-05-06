339ba2d47ee60e98baa7cfa0f4686386cda54692
Ella <ellacalugay@gmail.com>
2023-05-06 19:39:44 +0800

    Adding except ValueError

--- Simple App Calculator.py
+++ Simple App Calculator.py
@@ -3,44 +3,47 @@
 #Pseuodocode
 # Create a while loop with a True condition.
 while True:
-    # Ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication and Division).
-    operation = str(input("Please choose among the four operation - Addition, Subtraction, Multiplication, Division: "))
+    try:
+        # Ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication and Division).
+        operation = str(input("Please choose among the four operation - Addition, Subtraction, Multiplication, Division: "))
 
-    # Ask the user for two numbers
-    print("Kindly input two numbers.")
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+        # Ask the user for two numbers
+        print("Kindly input two numbers.")
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
 
-    # Initizalize result variable.
-    result = ()
+        # Initizalize result variable.
+        result = ()
 
-    # Perform the calculation based on the operation that the user wants
-    if operation == "Addition":
-        # If the operation is Addition, add the two numbers.
-        result = num1 + num2
-    elif operation == "Subtraction": 
-        # If the operation is Subtraction, subtract the second number from the first number
-        result = num1 - num2
-    elif operation == "Multiplication": 
-        # If the operation is Multiplication, multiply the two numbers.
-        result = num1 * num2
-    elif operation == "Division": 
-        # If the operation is Division, divide the first number by the second number.
-        result = num1 / num2
-    else:
-        print("Invalid Operation")
-        result = None
-    # Display the result
-    if result is not None:
-        print("Results:", result)
-        print("Operation completed.")
+        # Perform the calculation based on the operation that the user wants
+        if operation == "Addition":
+            # If the operation is Addition, add the two numbers.
+            result = num1 + num2
+        elif operation == "Subtraction": 
+            # If the operation is Subtraction, subtract the second number from the first number
+            result = num1 - num2
+        elif operation == "Multiplication": 
+            # If the operation is Multiplication, multiply the two numbers.
+            result = num1 * num2
+        elif operation == "Division": 
+            # If the operation is Division, divide the first number by the second number.
+            result = num1 / num2
+        else:
+            print("Invalid Operation")
+            result = None
+        # Display the result
+        if result is not None:
+            print("Results:", result)
+            print("Operation completed.")
 
-    # Ask if the user wants to try again or not.
-    # If yes, repeat the process.
-    try_again = input("Do you want to try again? (yes/no): ")
-    # If no, Display “Thank you!” and exit the program.
-    if try_again == "no":
-        print("Thank you.")
-        break # exit the loop
+        # Ask if the user wants to try again or not.
+        # If yes, repeat the process.
+        try_again = input("Do you want to try again? (yes/no): ")
+        # If no, Display “Thank you!” and exit the program.
+        if try_again == "no":
+            print("Thank you.")
+            break # exit the loop
 
-    # Use Python Function and appropriate Exceptions to capture errors during runtime.
+    #Use Python Function and appropriate Exceptions to capture errors during runtime.
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
